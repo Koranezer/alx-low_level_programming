73bf86f02a198dcd3c09182853a6ddf436f5a800
Koranezer <ndimkorahebenezer@gmail.com>
2022-12-09 06:11:02 -0800

    added project 0x01-variable direcory

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	int num, alpha;
+
+	for (num = 0; num <= 9; num++)
+		putchar((num % 10) + '0');
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
