fe8689730f6f23b8e5f057f5417ca0aa3c0d67b1
Koranezer <ndimkorahebenezer@gmail.com>
2022-12-13 17:20:14 -0800

    task number 2 to 11 added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase character
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is alphabet, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
